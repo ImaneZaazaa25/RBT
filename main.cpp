e3a10b86ab9b6c33dc2f2a0b033df3f65c5c8a66
imane <zaazaaimane24@gmail.com>
2025-12-03 10:05:36 +0100

    Modification du contenu du main

--- main.cpp
+++ main.cpp
@@ -40,12 +40,10 @@ Cel* rotationGauche(Cel* root, Cel* x) {
     Cel* y = x->droite;
     x->droite = y->gauche;
     if (y->gauche != nullptr) y->gauche->parent = x;
-
     y->parent = x->parent;
     if (x->parent == nullptr) root = y;
     else if (x == x->parent->gauche) x->parent->gauche = y;
     else x->parent->droite = y;
-
     y->gauche = x;
     x->parent = y;
     return root;
@@ -55,54 +53,42 @@ Cel* rotationDroite(Cel* root, Cel* x) {
     Cel* y = x->gauche;
     x->gauche = y->droite;
     if (y->droite != nullptr) y->droite->parent = x;
-
     y->parent = x->parent;
     if (x->parent == nullptr) root = y;
     else if (x == x->parent->gauche) x->parent->gauche = y;
     else x->parent->droite = y;
-
     y->droite = x;
     x->parent = y;
     return root;
 }
 
-/*****************************************************************************************
- Nom de fonction : correctionInsertion
- Description : Corrige l'arbre après insertion pour respecter les propriétés RBT.
-*****************************************************************************************/
 Cel* correctionInsertion(Cel* root, Cel* pt) {
     Cel* parent_pt = nullptr;
     Cel* grand_parent_pt = nullptr;
 
-    while (pt != root && pt->couleur == ROUGE && pt->parent->couleur == ROUGE) {
+    while (pt != root && pt->parent->couleur == ROUGE) {
         parent_pt = pt->parent;
         grand_parent_pt = parent_pt->parent;
 
-        // Parent est gauche du grand-parent
         if (parent_pt == grand_parent_pt->gauche) {
             Cel* uncle = grand_parent_pt->droite;
-
-            // Cas 1 : l'oncle est rouge
             if (uncle != nullptr && uncle->couleur == ROUGE) {
                 grand_parent_pt->couleur = ROUGE;
                 parent_pt->couleur = NOIR;
                 uncle->couleur = NOIR;
                 pt = grand_parent_pt;
             } else {
-                // Cas 2 : pt est droite
                 if (pt == parent_pt->droite) {
                     root = rotationGauche(root, parent_pt);
                     pt = parent_pt;
                     parent_pt = pt->parent;
                 }
-                // Cas 3 : pt est gauche
                 root = rotationDroite(root, grand_parent_pt);
                 swap(parent_pt->couleur, grand_parent_pt->couleur);
                 pt = parent_pt;
             }
-        } else { // Parent est droite du grand-parent
+        } else {
             Cel* uncle = grand_parent_pt->gauche;
-
             if (uncle != nullptr && uncle->couleur == ROUGE) {
                 grand_parent_pt->couleur = ROUGE;
                 parent_pt->couleur = NOIR;
@@ -126,33 +112,35 @@ Cel* correctionInsertion(Cel* root, Cel* pt) {
 
 /*****************************************************************************************
  Nom de fonction : insererCel
- Description : Insère un mot dans l'arbre RBT.
+ Description : Insère un nouveau mot dans l'arbre rouge-noir.
 *****************************************************************************************/
 Cel* insererCel(Cel* root, string val) {
     Cel* pt = creerCel(val);
 
-    // Insertion BST standard
+    // Recherche de la position d'insertion
     Cel* parent = nullptr;
     Cel* current = root;
     while (current != nullptr) {
         parent = current;
-        if (val < current->val)
+        if (val < current->val) {
             current = current->gauche;
-        else
+        } else {
             current = current->droite;
+        }
     }
 
+    // Insertion du nouveau nœud
     pt->parent = parent;
-    if (parent == nullptr)
-        root = pt;
-    else if (val < parent->val)
+    if (parent == nullptr) {
+        root = pt; // L'arbre était vide
+    } else if (val < parent->val) {
         parent->gauche = pt;
-    else
+    } else {
         parent->droite = pt;
+    }
 
-    // Correction des propriétés RBT
-    root = correctionInsertion(root, pt);
-    return root;
+    // Correction des propriétés rouge-noir
+    return correctionInsertion(root, pt);
 }
 
 /*****************************************************************************************
@@ -162,6 +150,7 @@ Cel* insererCel(Cel* root, string val) {
 void parcoursInfixe(Cel* racine) {
     if (racine != nullptr) {
         parcoursInfixe(racine->gauche);
+        // Décommentez la ligne suivante pour afficher les mots avec leur couleur
         //cout << racine->val << " (" << (racine->couleur == ROUGE ? "R" : "N") << ")" << endl;
         parcoursInfixe(racine->droite);
     }
@@ -176,11 +165,18 @@ Cel* insererDepuisFichier(const string& nomFichier) {
         cerr << "Erreur : impossible d'ouvrir le fichier " << nomFichier << endl;
         return nullptr;
     }
+
     Cel* racine = nullptr;
     string mot;
+
+
+    cout << "Insertion des mots depuis le fichier " << nomFichier << " :\n";
+
     while (fichier >> mot) {
         racine = insererCel(racine, mot);
+       // cout << "Mot inséré : " << mot << " (" << compteur << ")" << endl;
     }
+
     fichier.close();
     return racine;
 }
@@ -190,13 +186,17 @@ Cel* insererDepuisFichier(const string& nomFichier) {
 *****************************************************************************************/
 int main() {
     string nomFichier = "test1.txt";
+
+    cout << "=== Arbre Rouge-Noir - Insertion depuis fichier ===\n";
     Cel* racine = insererDepuisFichier(nomFichier);
+
     if (racine == nullptr) {
-        cout << "Aucun mot inséré." << endl;
+        cout << "Aucun mot inséré. Vérifiez le fichier " << nomFichier << endl;
         return 1;
     }
 
     cout << "\n=== Mots dans l'ordre alphabétique avec couleur ===\n";
     parcoursInfixe(racine);
+
     return 0;
 }
